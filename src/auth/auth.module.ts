b881b4aff9ac68277f58716701c83dbc86882122
Fernando Gomez <fernando.gomez@businessmind.es>
2023-08-21 14:56:00 +0200

    Add send token in login

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
 // import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
   // we create a global module in order to not import it in each module
   // imports: [PrismaModule],
+  imports: [JwtModule.register({})],
   controllers: [AuthController],
   providers: [AuthService]
 })
